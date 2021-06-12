5c705e455108bd2c28aeed8616b7ad50785e20d7
avinash.b <avinash.b@techsophy.com>
2021-06-13 00:40:14 +0530

    main layout grid basic

--- src/ChatApp/index.tsx
+++ src/ChatApp/index.tsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import {Sidebar, MessageList, AddMessage} from '../components';
+import './index.css'
 
 /* main chat window and the layout */
 
 class ChatApp extends Component {
   render() {
       return (
-        <div id="container">
-        <Sidebar />
-        <section id="main">
+        <div className="mainContainer">
+          <div className="sideBar">
+            <Sidebar />
+          </div>
+        <section className="main">
           <MessageList />
           <AddMessage />
         </section>
